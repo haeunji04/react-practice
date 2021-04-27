e86240ea72f8f4ed5a7ea4b45e4e30193570bf05
haeunji04 <ppippu12@gmail.com>
2021-04-27 23:45:36 +0900

    20210427 commit

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,3 +1,30 @@
-module.exports = {
+const path = require('path');
 
-}
+module.exports = {
+    name : 'wordrelay-setting',
+    mode: 'development', //실서비스에서는 production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    },  //입력
+
+    module: {
+        rules: [{
+           test: /\.jsx?$/,
+           loader: 'babel-loader',
+           options: {
+               presets: ['@babel/env', '@babel/react'],
+               plugins: ['@babel/plugin-proposal-class-properties'],
+           },
+        }],
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'), // \lecture\dist
+        filename: 'app.js'
+    },  //출력
+};
